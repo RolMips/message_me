79e0a524ef3311936f700cc3512d825668895935
Roland Martinet <r.martinet@nerya.fr>
2024-03-31 17:57:03 +0200

    style_and_navigation_(209_to_217)

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js", preload: true
+pin "semantic-ui", to: "https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.js"
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'chatroom#index'
+  get 'login', to: 'sessions#new'
+
 end
