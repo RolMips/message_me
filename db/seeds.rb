3a5860d2360ae766e916bb35a97e265b8b79cd5e
Roland Martinet <r.martinet@nerya.fr>
2024-04-07 23:53:53 +0200

    build_User_and_Message_resources_(221_to_223)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(username: "JohnDoe", password: "123Soleil")
+User.create(username: "Rolmips", password: "123Soleil")
+User.create(username: "JamesBond", password: "123Soleil")
+User.create(username: "RolandM", password: "123Soleil")
+User.create(username: "Isa", password: "123Soleil")
+Message.create(body: "Message JohnDoe", user_id: 1)
+Message.create(body: "Message Rolmips", user_id: 2)
+Message.create(body: "Message JamesBond", user_id: 3)
+Message.create(body: "Message RolandM", user_id: 4)
+Message.create(body: "Message Isa", user_id: 5)
